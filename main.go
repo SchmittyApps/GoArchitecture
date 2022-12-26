3c74ff54731b6138d9120b629551aad63aced48a
schmittyApps <117677136+SchmittyApps@users.noreply.github.com>
2022-12-26 17:28:13 +0000

    1228 up

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
-  "github.com/SchmittyApps/GoArchitecture/dirOne/dirThree"
-  "github.com/SchmittyApps/GoArchitecture/dirTwo"
+	"github.com/SchmittyApps/GoArchitecture/dirOne"
+	"github.com/SchmittyApps/GoArchitecture/dirOne/dirThree"
+	"github.com/SchmittyApps/GoArchitecture/dirTwo"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+	dirThree.SayHello()
+	dirOne.SayGoodbye()
+	dirTwo.SayShowdown()
 }
