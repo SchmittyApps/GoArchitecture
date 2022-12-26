f5125a503ab0cefb709660d856372036e9875ff4
schmittyApps <117677136+SchmittyApps@users.noreply.github.com>
2022-12-26 18:03:49 +0000

    1303 up

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/SchmittyApps/GoArchitecture
+module main
 
 go 1.19
+
+require github.com/SchmittyApps/GoArchitecture v0.0.0-20221226172813-3c74ff54731b
--- main.go
+++ main.go
@@ -5,11 +5,55 @@ import (
 	"github.com/SchmittyApps/GoArchitecture/dirOne"
 	"github.com/SchmittyApps/GoArchitecture/dirOne/dirThree"
 	"github.com/SchmittyApps/GoArchitecture/dirTwo"
+	"html/template"
+	"net/http"
+	"path"
+	"time"
 )
 
-func main() {
+func directory() {
 	fmt.Println("Hello, World!")
 	dirThree.SayHello()
 	dirOne.SayGoodbye()
 	dirTwo.SayShowdown()
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+
+	tmpl, err := template.ParseGlob("templates/*")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	name := ""
+	if r.URL.Path == "/" {
+		name = "index.html"
+	} else {
+		name = path.Base(r.URL.Path)
+	}
+
+	data := struct {
+		Time time.Time
+	}{
+		Time: time.Now(),
+	}
+
+	if err := tmpl.ExecuteTemplate(w, name, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		fmt.Println("error", err)
+	}
+}
+
+func main() {
+	fmt.Println("http server up!")
+	http.Handle(
+		"/static/",
+		http.StripPrefix(
+			"/static/",
+			http.FileServer(http.Dir("static")),
+		),
+	)
+	http.HandleFunc("/", handle)
+	http.ListenAndServe(":0", nil)
 }
